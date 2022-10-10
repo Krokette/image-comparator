3b83ed5bcbea0fc20bc7e6d28592c59520c8efde
Krokette <65606052+Krokette@users.noreply.github.com>
2022-10-11 01:36:31 +0200

    add copy paste feature - a bit wonky, but it works

--- js/comparison.js
+++ js/comparison.js
@@ -9,22 +9,25 @@ function initComparisons() {
   }
   function compareImages(img) {
     cleaningHTML();
-    var slider, img, clicked = 0, timesclicked=0, w, h;
+    var slider,
+      img,
+      clicked = 0,
+      timesclicked = 0,
+      w,
+      h;
     /*get the width and height of the img element*/
     w = img.offsetWidth;
     h = img.offsetHeight;
-      console.log(h);
     /*set the width of the img element to 50%:*/
-    img.style.width = (w / 2) + "px";
+    img.style.width = w / 2 + "px";
     /*create slider:*/
     slider = document.createElement("DIV");
     slider.setAttribute("class", "img-comp-slider");
-    slider.style.height=h; //the same height as the images
     /*insert slider*/
     img.parentElement.insertBefore(slider, img);
     /*position the slider in the middle:*/
-    slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-    slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+    slider.style.top = h / 2 - slider.offsetHeight / 2 + "px";
+    slider.style.left = w / 2 - slider.offsetWidth / 2 + "px";
     /*execute a function when the mouse button is pressed:*/
     slider.addEventListener("click", slideReady);
     /*and another function when the mouse button is released:*/
@@ -43,12 +46,12 @@ function initComparisons() {
       window.addEventListener("touchmove", slideMove);
     }
     function slideFinish() {
-      if (timesclicked >= 1){
-          /*the slider is no longer clicked:*/
-          clicked = 0;
-          timesclicked=0;
-      }else{
-          timesclicked++;
+      if (timesclicked >= 1) {
+        /*the slider is no longer clicked:*/
+        clicked = 0;
+        timesclicked = 0;
+      } else {
+        timesclicked++;
       }
     }
     function slideMove(e) {
@@ -56,7 +59,7 @@ function initComparisons() {
       /*if the slider is no longer clicked, exit this function:*/
       if (clicked == 0) return false;
       /*get the cursor's x position:*/
-      pos = getCursorPos(e)
+      pos = getCursorPos(e);
       /*prevent the slider from being positioned outside the image:*/
       if (pos < 0) pos = 0;
       if (pos > w) pos = w;
@@ -64,7 +67,8 @@ function initComparisons() {
       slide(pos);
     }
     function getCursorPos(e) {
-      var a, x = 0;
+      var a,
+        x = 0;
       e = e || window.event;
       /*get the x positions of the image:*/
       a = img.getBoundingClientRect();
@@ -78,19 +82,18 @@ function initComparisons() {
       /*resize the image:*/
       img.style.width = x + "px";
       /*position the slider:*/
-      slider.style.left = img.offsetWidth - (slider.offsetWidth/2) + "px";
+      slider.style.left = img.offsetWidth - slider.offsetWidth / 2 + "px";
     }
   }
 }
 
-function cleaningHTML(){
-    
-    try{
-        $('.img-comp-slider').remove();
-        console.log("cleaning...");
-    }
-    catch(err){
-        
-    }
-    
+function cleaningHTML() {
+  try {
+    //$('.img-comp-slider').remove();
+    console.log("cleaning...");
+    /*
+        var x = document.getElementsByClassName("img-comp-slider");
+        x.remove();
+        */
+  } catch (err) {}
 }
--- js/files.js
+++ js/files.js
@@ -1,9 +1,76 @@
-var loadFileBefore = function(event) {
-	var image = document.getElementById('before');
-	image.src = URL.createObjectURL(event.target.files[0]);
+var loadFileBefore = function (event) {
+  let imageBefore = document.getElementById("before");
+  imageBefore.src = URL.createObjectURL(event.target.files[0]);
 };
-var loadFileAfter = function(event) {
-	var image = document.getElementById('after');
-	image.src = URL.createObjectURL(event.target.files[0]);
-    initComparisons();
+var loadFileAfter = function (event) {
+  let imageAfter = document.getElementById("after");
+  imageAfter.src = URL.createObjectURL(event.target.files[0]);
+  initComparisons();
 };
+
+function reset() {
+  let imageBefore = document.getElementById("before");
+  let imageAfter = document.getElementById("after");
+  imageBefore.src = "";
+  imageAfter.src = "";
+  location.reload();
+}
+
+
+window.onload = function() {
+	let contentTargetBefore = document.getElementById("pasteContainerBefore");   
+	
+	contentTargetBefore.onpaste = (e) => {                                                  // When there's an paste event on our target DIV:
+	   let cbPayload = [...(e.clipboardData || e.originalEvent.clipboardData).items];     // Capture the ClipboardEvent's eventData payload as an array
+		   cbPayload = cbPayload.filter(i => /image/.test(i.type));                       // Strip out the non-image bits
+	   
+	   if(!cbPayload.length || cbPayload.length === 0) return false;                      // If no image was present in the collection, bail.
+	   
+	   let imageBefore = document.getElementById("before");
+	   imageBefore.src = URL.createObjectURL(cbPayload[0].getAsFile());                    // ... then read in the pasteboard image data as Base64
+
+	   //    let reader = new FileReader();                                                     // Instantiate a FileReader...
+	   //    reader.onload = (e) => contentTargetBefore.innerHTML = `<img src="${e.target.result}">`; // ... set its onLoad to render the event target's payload
+	   //    reader.readAsDataURL(cbPayload[0].getAsFile());   
+	};
+	
+
+	let contentTargetAfter = document.getElementById("pasteContainerAfter");               // Target our DIV's DOM node.
+	
+	contentTargetAfter.onpaste = (e) => {                                                      // When there's an paste event on our target DIV:
+	   let cbPayload = [...(e.clipboardData || e.originalEvent.clipboardData).items];    	 // Capture the ClipboardEvent's eventData payload as an array
+		   cbPayload = cbPayload.filter(i => /image/.test(i.type));                      	 // Strip out the non-image bits
+	   
+	   if(!cbPayload.length || cbPayload.length === 0) return false;						// If no image was present in the collection, bail.
+	   
+	   let imageAfter = document.getElementById("after");
+	   imageAfter.src = URL.createObjectURL(cbPayload[0].getAsFile());  
+
+	//    let reader = new FileReader();                                                     // Instantiate a FileReader...
+	//    reader.onload = (e) => contentTargetAfter.innerHTML = `<img src="${e.target.result}">`; // ... set its onLoad to render the event target's payload
+	//    reader.readAsDataURL(cbPayload[0].getAsFile());                                    // ... then read in the pasteboard image data as Base64
+	};
+}
+
+
+
+
+//another idea from  https://www.techiedelight.com/paste-image-from-clipboard-using-javascript/
+// document.onpaste = function (pasteEvent) {
+//   // consider the first item (can be easily extended for multiple items)
+//   var item = pasteEvent.clipboardData.items[0];
+
+//   if (item.type.indexOf("image") === 0) {
+//     var blob = item.getAsFile();
+
+//     var reader = new FileReader();
+//     reader.onload = function (event) {
+//       document.getElementById("container").src = event.target.result;
+// 	  let imageBefore = document.getElementById("before");
+// 	  imageBefore.src = URL.createObjectURL(blob);
+//     };
+
+//     reader.readAsDataURL(blob);
+//   }
+// };
+
